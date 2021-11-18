30c5a1eafb85134282e9cbbc83481937bce5af93
realguyman <realguyman@protonmail.com>
2021-11-18 12:50:23 -0900

    Add Initializer and main entrypoint

---
+++ src/main/java/io/github/realguyman/steel/Initializer.java
@@ -0,0 +1,10 @@
+package io.github.realguyman.steel;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Initializer implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Steel initialized.");
+    }
+}
