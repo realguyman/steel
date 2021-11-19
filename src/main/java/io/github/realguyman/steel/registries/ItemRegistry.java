e85f55a5d6062513ffccef03f1c4759daa0806b5
realguyman <realguyman@protonmail.com>
2021-11-18 22:38:45 -0900

    Move steel ingot item to variable

--- src/main/java/io/github/realguyman/steel/registries/ItemRegistry.java
+++ src/main/java/io/github/realguyman/steel/registries/ItemRegistry.java
@@ -7,11 +7,15 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 
 public class ItemRegistry {
+    public static final Item STEEL_INGOT = new Item(
+        new Item.Properties().tab(CreativeModeTab.TAB_MISC)
+    );
+
     public static void register() {
         Registry.register(
             Registry.ITEM,
             new ResourceLocation(Initializer.MOD_ID, "steel_ingot"),
-            new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC))
+            STEEL_INGOT
         );
     }
 }
