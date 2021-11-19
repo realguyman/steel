19c1652a8abf0a75550ced950b7fb4d161d8201a
realguyman <realguyman@protonmail.com>
2021-11-18 22:35:35 -0900

    Simplify steel ingot registry

--- src/main/java/io/github/realguyman/steel/items/SteelIngotItem.java
+++
@@ -1,9 +0,0 @@
-package io.github.realguyman.steel.items;
-
-import net.minecraft.world.item.Item;
-
-public class SteelIngotItem extends Item {
-    public SteelIngotItem(Properties properties) {
-        super(properties);
-    }
-}
--- src/main/java/io/github/realguyman/steel/registries/ItemRegistry.java
+++ src/main/java/io/github/realguyman/steel/registries/ItemRegistry.java
@@ -1,20 +1,17 @@
 package io.github.realguyman.steel.registries;
 
 import io.github.realguyman.steel.Initializer;
-import io.github.realguyman.steel.items.SteelIngotItem;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 
 public class ItemRegistry {
     public static void register() {
         Registry.register(
             Registry.ITEM,
             new ResourceLocation(Initializer.MOD_ID, "steel_ingot"),
-            new SteelIngotItem(
-                new FabricItemSettings().tab(CreativeModeTab.TAB_MISC)
-            )
+            new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC))
         );
     }
 }
