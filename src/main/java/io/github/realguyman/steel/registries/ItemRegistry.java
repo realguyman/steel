9d4f27fad4cfe14fff6a3718c1ded8184ee8c939
realguyman <realguyman@protonmail.com>
2021-11-21 00:22:00 -0900

    Add steel nugget

--- src/main/java/io/github/realguyman/steel/registries/ItemRegistry.java
+++ src/main/java/io/github/realguyman/steel/registries/ItemRegistry.java
@@ -5,8 +5,12 @@ import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-public class ItemRegistry {
+public class ItemRegistry {    
     public static final Item STEEL_INGOT = new Item(
+        new Item.Settings().group(Initializer.ITEM_GROUP)
+    );
+
+    public static final Item STEEL_NUGGET = new Item(
         new Item.Settings().group(Initializer.ITEM_GROUP)
     );
 
@@ -15,6 +19,12 @@ public class ItemRegistry {
             Registry.ITEM,
             new Identifier(Initializer.MOD_ID, "steel_ingot"),
             STEEL_INGOT
+        );
+
+        Registry.register(
+            Registry.ITEM,
+            new Identifier(Initializer.MOD_ID, "steel_nugget"),
+            STEEL_NUGGET
         );
     }
 }
