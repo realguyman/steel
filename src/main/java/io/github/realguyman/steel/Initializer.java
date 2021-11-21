0cc96031e738b9187671f3c7d2e912d5df0c5618
realguyman <realguyman@protonmail.com>
2021-11-20 15:11:44 -0900

    Add steel item group

--- src/main/java/io/github/realguyman/steel/Initializer.java
+++ src/main/java/io/github/realguyman/steel/Initializer.java
@@ -2,9 +2,18 @@ package io.github.realguyman.steel;
 
 import io.github.realguyman.steel.registries.ItemRegistry;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 
 public class Initializer implements ModInitializer {
     public static final String MOD_ID = "steel";
+
+    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
+        new Identifier(MOD_ID, "all"),
+        () -> new ItemStack(ItemRegistry.STEEL_INGOT)
+    );
 
     @Override
     public void onInitialize() {
--- src/main/java/io/github/realguyman/steel/registries/ItemRegistry.java
+++ src/main/java/io/github/realguyman/steel/registries/ItemRegistry.java
@@ -2,13 +2,12 @@ package io.github.realguyman.steel.registries;
 
 import io.github.realguyman.steel.Initializer;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ItemRegistry {
     public static final Item STEEL_INGOT = new Item(
-        new Item.Settings().group(ItemGroup.MATERIALS)
+        new Item.Settings().group(Initializer.ITEM_GROUP)
     );
 
     public static void register() {
