9b30beb9da0e6389f6c38ae4e251f8b88e374c35
realguyman <realguyman@protonmail.com>
2021-11-20 23:28:57 -0900

    Add steel ingot to treasure chest loot tables

--- src/main/java/io/github/realguyman/steel/Initializer.java
+++ src/main/java/io/github/realguyman/steel/Initializer.java
@@ -3,12 +3,35 @@ package io.github.realguyman.steel;
 import io.github.realguyman.steel.registries.ItemRegistry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.loot.UniformLootTableRange;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.util.Identifier;
 
 public class Initializer implements ModInitializer {
     public static final String MOD_ID = "steel";
+
+    private static final String[] CHEST_LOOT_TABLES = {
+        "abandoned_mineshaft",
+        "buried_treasure",
+        "desert_pyramid",
+        "end_city_treasure",
+        "jungle_temple",
+        "nether_bridge",
+        "pillager_outpost",
+        "shipwreck_treasure",
+        "simple_dungeon",
+        "underwater_ruin_big",
+        "underwater_ruin_small",
+        "village/village_armorer",
+        "village/village_toolsmith",
+        "village/village_weaponsmith",
+        "woodland_mansion"
+    };
 
     public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
         new Identifier(MOD_ID, "all"),
@@ -18,5 +41,24 @@ public class Initializer implements ModInitializer {
     @Override
     public void onInitialize() {
         ItemRegistry.register();
+
+        Identifier chestLootTableIdentifiers[] = new Identifier[CHEST_LOOT_TABLES.length];
+
+        for (int i = 0; i < CHEST_LOOT_TABLES.length; i++) {
+            chestLootTableIdentifiers[i] = new Identifier("minecraft:chests/" + CHEST_LOOT_TABLES[i]);
+        }
+
+        LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, table, setter) -> {
+            for (Identifier chestLootTableIdentifier : chestLootTableIdentifiers) {
+                if (chestLootTableIdentifier.equals(id)) {
+                    table.withPool(
+                        FabricLootPoolBuilder.builder()
+                        .withRolls(UniformLootTableRange.between(0, 2))
+                        .withFunction(SetCountLootFunction.builder(UniformLootTableRange.between(1, 3)))
+                        .withEntry(ItemEntry.builder(ItemRegistry.STEEL_INGOT))
+                    );
+                }
+            }
+        });
     }
 }
