9d278c4f3ddd268e3959d0889bd0da2cd2e682bd
realguyman <realguyman@protonmail.com>
2021-11-22 19:26:54 -0900

    Add steel armor

---
+++ src/main/java/io/github/realguyman/steel/SteelArmorMaterial.java
@@ -0,0 +1,50 @@
+package io.github.realguyman.steel;
+
+import io.github.realguyman.steel.registry.ItemRegistry;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+
+public class SteelArmorMaterial implements ArmorMaterial {
+    private static final int[] DURABILITY = {13, 15, 16, 11};
+    private static final int[] PROTECTION = {2, 5, 7, 2};
+    
+    public static final SteelArmorMaterial INSTANCE = new SteelArmorMaterial();
+
+    @Override
+    public int getDurability(EquipmentSlot slot) {
+        return DURABILITY[slot.getEntitySlotId()] * 25;
+    }
+    
+    @Override
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return PROTECTION[slot.getEntitySlotId()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 15;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return SoundEvents.ITEM_ARMOR_EQUIP_IRON;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(ItemRegistry.STEEL_INGOT);
+    }
+
+    @Override
+    public String getName() {
+        return "steel";
+    }
+
+    @Override
+    public float getToughness() {
+        return 1;
+    }
+}
