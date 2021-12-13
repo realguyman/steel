e3716ac2ddda3bf5d6b089406037eda2db29ec8c
realguyman <realguyman@protonmail.com>
2021-12-12 17:15:24 -0900

    Refactor steel block registry

--- src/main/java/io/github/realguyman/steel/registry/BlockRegistry.java
+++ src/main/java/io/github/realguyman/steel/registry/BlockRegistry.java
@@ -4,15 +4,13 @@ import io.github.realguyman.steel.Initializer;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
-import net.minecraft.block.Material;
-import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class BlockRegistry {
     public static final Block STEEL_BLOCK = new Block(
-        FabricBlockSettings.of(Material.METAL).strength(5, 6).sounds(BlockSoundGroup.METAL)
-        .breakByTool(FabricToolTags.PICKAXES, 2)
+        FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).breakByTool(FabricToolTags.PICKAXES, 2)
     );
 
     public static void register() {
